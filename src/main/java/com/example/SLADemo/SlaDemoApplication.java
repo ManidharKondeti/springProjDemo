77c144aafa0531f698e8c7b3c1d83d77a1132519
ManidharKondeti <manidharbabukondeti@gmail.com>
2023-11-27 20:42:07 -0600

    Update SlaDemoApplication.java

--- src/main/java/com/example/SLADemo/SlaDemoApplication.java
+++ src/main/java/com/example/SLADemo/SlaDemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.SLADemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
