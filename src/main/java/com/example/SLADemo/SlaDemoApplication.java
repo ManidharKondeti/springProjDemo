9497877b9ee8a409267dd1324fbea211070c33df
ManidharKondeti <manidharbabukondeti@gmail.com>
2023-11-27 20:22:35 -0600

    Code

---
+++ src/main/java/com/example/SLADemo/SlaDemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.SLADemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SlaDemoApplication {
+        
+       @GetMapping("/msg")
+       public String message(){
+           return "Testing the Azure Deployment";
+       }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SLADemo/SlaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
